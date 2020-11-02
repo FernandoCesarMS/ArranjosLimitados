f3a5aedb3712f418adec6480f863a7f38ba413ef
Fernando Cesar <cmnando.silva@gmail.com>
2020-11-02 16:03:23 -0300

    Classe para Erro de não inicialização criada

--- main.cpp
+++ main.cpp
@@ -1,50 +1,83 @@
 #include <iostream>
+#include <exception>
+#include <string.h>
+using namespace std;
+class erroIndiceInicio : public exception
+{
+protected:
+  char msg[100];
 
-template <class T, int N> class BoundedArray {
-  public:
-    void set(int index, T value) {
-      buf[index] = value;
-    }
-    T get(int index) {
-      return buf[index];
-    }
-  private:
-    T buf[N];
+public:
+  erroIndiceInicio(const char *e)
+  {
+    strcpy(msg, e);
+  }
+  virtual const char *what()
+  {
+    return msg;
+  }
+};
+template <class T, int N>
+class BoundedArray
+{
+public:
+  void set(int index, T value)
+  {
+    buf[index] = value;
+  }
+  T get(int index)
+  {
+    return buf[index];
+  }
+
+private:
+  T buf[N];
 };
 
-template <class T> void testArray() {
+template <class T>
+void testArray()
+{
   BoundedArray<T, 8> a;
   char action;
-  while (std::cin >> action) {
+  while (std::cin >> action)
+  {
     int index;
     std::cin >> index;
-    try {
-      if (action == 's') {
+    try
+    {
+      if (action == 's')
+      {
         T value;
         std::cin >> value;
         a.set(index, value);
-      } else if (action == 'g') {
+      }
+      else if (action == 'g')
+      {
         std::cout << a.get(index) << std::endl;
       }
-    } catch (...) {
+    }
+    catch (...)
+    {
       std::cerr << "Erro desconhecido." << std::endl;
     }
   }
 }
 
-int main() {
+int main()
+{
   char type;
   std::cin >> type;
-  switch(type) {
-    case 'd':
-      testArray<double>();
-      break;
-    case 'i':
-      testArray<int>();
-      break;
-    case 's':
-      testArray<std::string>();
-      break;
+  switch (type)
+  {
+  case 'd':
+    testArray<double>();
+    break;
+  case 'i':
+    testArray<int>();
+    break;
+  case 's':
+    testArray<std::string>();
+    break;
   }
   return 0;
 }
